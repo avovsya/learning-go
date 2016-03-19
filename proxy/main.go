463dff63369b1758f43cde7d4135370d3e4b1aa8
Artem Vovsia <avovsya@gmail.com>
2016-03-19 16:25:35 +0000

    Add connection pool

--- proxy/main.go
+++ proxy/main.go
@@ -1,5 +1,14 @@
 package main
 
+/*
+*	TODO
+* [ ] Keep backend connection pull warn
+* [ ] Get rid of old backend connections in the pull
+* [ ] Prettify code(separate to multiple files)
+* [ ] Write benchmark
+* [ ] Compare to https://github.com/zorkian/lca2015/tree/master/final
+ */
+
 import (
 	"bufio"
 	"io"
@@ -9,13 +18,48 @@ import (
 	"os"
 	"strconv"
 	"sync"
+	"time"
 )
 
+type Backend struct {
+	net.Conn
+	Reader *bufio.Reader
+	Writer *bufio.Writer
+}
+
+var backendQueue chan *Backend
 var requestBytes map[string]int64
 var requestLock sync.Mutex
 
 func init() {
 	requestBytes = make(map[string]int64)
+	backendQueue = make(chan *Backend, 10)
+}
+
+func queueBackend(be *Backend) {
+	select {
+	case backendQueue <- be:
+	case <-time.After(1 * time.Second):
+		be.Close()
+	}
+}
+
+func getBackend() (*Backend, error) {
+	select {
+	case be := <-backendQueue:
+		return be, nil
+	case <-time.After(100 * time.Millisecond):
+		be, err := net.Dial("tcp", "127.0.0.1:8081")
+		if err != nil {
+			return nil, err
+		}
+
+		return &Backend{
+			Conn:   be,
+			Reader: bufio.NewReader(be),
+			Writer: bufio.NewWriter(be),
+		}, nil
+	}
 }
 
 func updateStats(req *http.Request, resp *http.Response) int64 {
@@ -39,19 +83,31 @@ func handleConnection(conn net.Conn) {
 			}
 			return
 		}
-		if backendConn, err := net.Dial("tcp", "127.0.0.1:8081"); err == nil {
-			backend_reader := bufio.NewReader(backendConn)
-			if err := req.Write(backendConn); err == nil {
-				if resp, err := http.ReadResponse(backend_reader, req); err == nil {
-					bytes := updateStats(req, resp)
-					resp.Header.Set("X-Bytes", strconv.FormatInt(bytes, 10))
-					resp.Close = true
-					if err := resp.Write(conn); err == nil {
-						log.Printf("%s: %d, %d bytes", req.URL.Path, resp.StatusCode, requestBytes[req.URL.Path])
-					}
+
+		be, err := getBackend()
+		if err != nil {
+			return
+		}
+
+		if err := req.Write(be.Writer); err == nil {
+			be.Writer.Flush()
+			if resp, err := http.ReadResponse(be.Reader, req); err == nil {
+				respWriter := bufio.NewWriter(conn)
+				bytes := updateStats(req, resp)
+				resp.Header.Set("X-Bytes", strconv.FormatInt(bytes, 10))
+				resp.Close = true
+				if err := resp.Write(respWriter); err == nil {
+					respWriter.Flush()
+					log.Printf("%s: %d, %d bytes", req.URL.Path, resp.StatusCode, bytes)
+				}
+
+				if resp.Close {
+					return
 				}
 			}
 		}
+
+		go queueBackend(be)
 	}
 }
 
